e54278c60476eec669376001f1f4e406d92456ce
haesiku <haesiku@gmail.com>
2020-04-28 10:59:17 +0900

    first commit

---
+++ src/main/java/com/example/demo/Order.java
@@ -0,0 +1,83 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.util.MimeTypeUtils;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="ORDER_TABLE")
+public class Order {
+
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long productId;
+    private String productName;
+    private int productQty;
+
+    public int getProductQty() {
+        return productQty;
+    }
+
+    public void setProductQty(int productQty) {
+        this.productQty = productQty;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    @PostPersist
+    public void eventPublish(){
+        OrderPlaced orderPlaced = new OrderPlaced();
+        orderPlaced.setOrderId(this.getId());
+        orderPlaced.setProductId(this.getProductId());
+        orderPlaced.setProductName(this.getProductName());
+        orderPlaced.setProductQty(this.getProductQty());
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = null;
+
+        try {
+            json = objectMapper.writeValueAsString(orderPlaced);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("JSON format exception", e);
+        }
+        System.out.println("Order Replaced : " + json);
+
+        Processor processor = DemoApplication.applicationContext.getBean(Processor.class);
+        MessageChannel outputChannel = processor.output();
+
+        outputChannel.send(MessageBuilder
+                .withPayload(json)
+                .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
+                .build());
+    }
+
+}
